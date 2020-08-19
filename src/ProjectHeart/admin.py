48cd9ae3a783ffb03bf4de3b46e4c789c49fcb4a
Plays Lokotama <lokotamathemastermind@gmail.com>
2020-08-19 18:00:47 +0100

    Added support for built-in stylesheets | Added models for the password storage | Add the Administrator panel for managing the passwords

--- src/ProjectHeart/admin.py
+++ src/ProjectHeart/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Passwords
 
-# Register your models here.
+class PasswordsAdmin(admin.ModelAdmin):
+    list_display = ['user', 'website_name', 'website_url', 'password_hashed']
+    fields = ['user', 'website_name', 'website_url', 'password']
+    list_filter = ['user', 'website_url']
+
+admin.site.register(Passwords, PasswordsAdmin)
--- src/ProjectHeart/models.py
+++ src/ProjectHeart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
+class Passwords(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    website_name = models.CharField(max_length=100, default="Example Site", blank=False, null=False, verbose_name="Site Name", unique=True)
+    website_url = models.URLField(blank=False, null=False, max_length=100, verbose_name="Site URI", default="https://www.domain.com/")
+    password = models.CharField(max_length=1000, blank=True, null=True, verbose_name="Password (Actual)", unique=True)
+    password_hashed = models.CharField(max_length=1500, null=True, blank=True, default="No hashed password", verbose_name="Password (Hashed)")
+
+    class Meta:
+        verbose_name_plural = "Passwords"
+
+    def __str__(self):
+        return self.website_name
+
+    def save(self, *args, **kwargs):
+        if self.password:
+            pwd = make_password(self.password)[:30]
+        self.password_hashed = str(pwd)
+        super().save(*args, **kwargs)
