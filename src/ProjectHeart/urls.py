910919f7d5b7caea8dbc4204467d8361d2b99ed9
Plays Lokotama <lokotamathemastermind@gmail.com>
2020-09-07 12:12:18 +0100

    Edited the ProjectHeart & PasswordVault folders

--- src/ProjectHeart/urls.py
+++ src/ProjectHeart/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, profile
 
 urlpatterns = [
-    path('', index, name='home-page')
+    path('', index, name='home-page'),
+    path('profile/', profile, name='profile-page'),
 ]
--- src/ProjectHeart/views.py
+++ src/ProjectHeart/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from .forms import PasswordsForm
 from .models import Passwords
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     # user = Passwords.objects.get(user=request.user)
     form = PasswordsForm(request.POST or None)
+    
     context = {
         "form": form
     }
     return render(request, "home/home.html", context)
 
+@login_required
 def profile(request):
     context = {
 
