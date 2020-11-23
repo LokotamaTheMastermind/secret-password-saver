fec5ff36bfbe0c7daec1e1ddd358919e5dfd59ae
Plays Lokotama <lokotamathemastermind@gmail.com>
2020-11-23 15:02:14 +0100

    Added project requirements and some static files

---
+++ src/static/shared/js/script.js
@@ -0,0 +1,8 @@
+function togglePassword() {
+    var x = document.getElementById("id_password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
