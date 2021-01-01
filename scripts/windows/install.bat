f780a4e7c900c7d14c64e8ced0099ecec6318411
LokotamaTheMastermind <lokotamathemastermind2@gmail.com>
2021-01-01 22:28:29 +0100

    Edited some scripts

--- scripts/windows/install.bat
+++ scripts/windows/install.bat
@@ -1,4 +1,4 @@
 @REM You must have Anaconda installed and activated for this to work
 
-cd ../../ && conda create --name discord-friends --file requirements.txt python=3.7
-conda activate discord-friends && cd scripts/windows && setup.bat
+cd ../../ && conda env create -f environment.yml python=3.7
+conda activate credential-saver && cd scripts/windows && setup.bat
